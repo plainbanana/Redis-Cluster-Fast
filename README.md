6e4bba3b9352c56b60e1811a700d92fe9346e220
plainbanana <plainbanana@mustardon.tokyo>
2023-02-01 11:12:48 +0900

    docs: update pod

--- README.md
+++ README.md
@@ -1,14 +1,71 @@
 # NAME
 
-Redis::Cluster::Fast - It's new $module
+Redis::Cluster::Fast - A fast perl binding for Redis Cluster
 
 # SYNOPSIS
 
     use Redis::Cluster::Fast;
 
+    my $redis = Redis::Cluster::Fast->new(
+        startup_nodes => [
+            'localhost:9000',
+            'localhost:9001',
+            'localhost:9002',
+            'localhost:9003',
+            'localhost:9004',
+            'localhost:9005',
+        ],
+        connect_timeout => 0.05,
+        command_timeout => 0.05.,
+        max_retry => 10,
+    );
+
+    # 'OK'
+    my $res = $redis->set('test', 123);
+    # '123'
+    my $str = $redis->get('test');
+    # 'OK'
+    $res = $redis->mset('{my}foo', 'hoge', '{my}bar', 'fuga');
+    # get as array
+    my @array = $redis->mget('{my}foo', '{my}bar');
+    # get as array-ref
+    my $array_ref = $redis->mget('{my}foo', '{my}bar');
+
 # DESCRIPTION
 
-Redis::Cluster::Fast is ...
+Redis::Cluster::Fast is like [Redis::Fast](https://github.com/shogo82148/Redis-Fast) but support Redis Cluster by [hiredis-cluster](https://github.com/Nordix/hiredis-cluster).
+
+Require Redis 6 or higher to support [RESP3](https://github.com/antirez/RESP3/blob/master/spec.md).
+
+# METHODS
+
+## new(%args)
+
+Following arguments are available.
+
+- startup\_nodes
+
+    Specifies the list of Redis Cluster nodes.
+
+- connect\_timeout
+
+    A fractional seconds. (default: 10)
+
+    Connection timeout to connect to a Redis node.
+
+- command\_timeout
+
+    A fractional seconds. (default: 10)
+
+    Redis Command execution timeout.
+
+- max\_retry
+
+    A integer value. (default: 10)
+
+## &lt;command>(@args)
+
+To run Redis command with arguments.
 
 # LICENSE
 
--- lib/Redis/Cluster/Fast.pm
+++ lib/Redis/Cluster/Fast.pm
@@ -66,15 +66,76 @@ __END__
 
 =head1 NAME
 
-Redis::Cluster::Fast - It's new $module
+Redis::Cluster::Fast - A fast perl binding for Redis Cluster
 
 =head1 SYNOPSIS
 
     use Redis::Cluster::Fast;
 
+    my $redis = Redis::Cluster::Fast->new(
+        startup_nodes => [
+            'localhost:9000',
+            'localhost:9001',
+            'localhost:9002',
+            'localhost:9003',
+            'localhost:9004',
+            'localhost:9005',
+        ],
+        connect_timeout => 0.05,
+        command_timeout => 0.05.,
+        max_retry => 10,
+    );
+
+    # 'OK'
+    my $res = $redis->set('test', 123);
+    # '123'
+    my $str = $redis->get('test');
+    # 'OK'
+    $res = $redis->mset('{my}foo', 'hoge', '{my}bar', 'fuga');
+    # get as array
+    my @array = $redis->mget('{my}foo', '{my}bar');
+    # get as array-ref
+    my $array_ref = $redis->mget('{my}foo', '{my}bar');
+
 =head1 DESCRIPTION
 
-Redis::Cluster::Fast is ...
+Redis::Cluster::Fast is like L<Redis::Fast|https://github.com/shogo82148/Redis-Fast> but support Redis Cluster by L<hiredis-cluster|https://github.com/Nordix/hiredis-cluster>.
+
+Require Redis 6 or higher to support L<RESP3|https://github.com/antirez/RESP3/blob/master/spec.md>.
+
+=head1 METHODS
+
+=head2 new(%args)
+
+Following arguments are available.
+
+=over 1
+
+=item startup_nodes
+
+Specifies the list of Redis Cluster nodes.
+
+=item connect_timeout
+
+A fractional seconds. (default: 10)
+
+Connection timeout to connect to a Redis node.
+
+=item command_timeout
+
+A fractional seconds. (default: 10)
+
+Redis Command execution timeout.
+
+=item max_retry
+
+A integer value. (default: 10)
+
+=back
+
+=head2 <command>(@args)
+
+To run Redis command with arguments.
 
 =head1 LICENSE
 
